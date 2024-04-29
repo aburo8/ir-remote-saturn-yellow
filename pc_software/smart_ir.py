1c098e60df0e31938316825f13c94998607eebeb
Alex B <abwork2071@gmail.com>
2024-04-30 09:05:38 +1000

    Basic GUI data structures & functionality. Working appliance ADD. Connected GUI components.

--- pc_software/smart_ir.py
+++ pc_software/smart_ir.py
@@ -1,3 +1,7 @@
+"""
+Graphical User Interface for the Smart-Ir: Universal Remote Control System
+Written by AB
+"""
 import struct
 import sys
 import os
@@ -25,12 +29,16 @@ import json
 from datetime import datetime
 import threading
 import pc_pb2
+from smart_ir_data import Control, Appliance, ControllerConfig, VERSION, generate_base_appliance
 
 # Global Variables
 portScanningState = False
 connectionStatus = 1  # 1 represent no connection; 2 represent connected sucessfully; 3 represent disconnect manually
 
 class MainWindow(QMainWindow):
+    # Class Private Variables
+    configData: ControllerConfig
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -44,10 +52,26 @@ class MainWindow(QMainWindow):
         self.scanPortsB = self.findChild(QPushButton, "ScanPorts_B")
         self.portSelectorC = self.findChild(QComboBox, "PortSelector_C")
         self.connectB = self.findChild(QPushButton, "connectB")
+        self.addApplianceB = self.findChild(QPushButton, "applianceAdd_b")
+        self.addApplianceNameT = self.findChild(QTextEdit, "applianceNameAdd_t")
+        self.updateControllersB = self.findChild(QPushButton, "updateControllers_b")
+        self.selectedApplianceC = self.findChild(QComboBox, "selectedAppliance_c")
+        self.controlLabelT = self.findChild(QTextEdit, "controlLabel_t")
+        self.irCodeT = self.findChild(QTextEdit, "irCode_t")
+        self.colourSelectB = self.findChild(QPushButton, "colourSelector_b")
+        self.orientationC = self.findChild(QComboBox, "orientation_c")
+        self.deleteApplianceB = self.findChild(QPushButton, "delAppliance_b")
+        self.controlB_1 = self.findChild(QPushButton, "control1_B")
+        self.controlB_2 = self.findChild(QPushButton, "control2_B")
+        self.controlB_3 = self.findChild(QPushButton, "control3_B")
+        self.controlB_4 = self.findChild(QPushButton, "control4_B")
+        self.controlB_5 = self.findChild(QPushButton, "control5_B")
+        self.controlB_6 = self.findChild(QPushButton, "control6_B")
 
         # Configure Buttons 
         self.connectB.clicked.connect(self.connect_to_port)
         self.scanPortsB.clicked.connect(self.serial_scan_handler)
+        self.addApplianceB.clicked.connect(self.add_appliance)
 
         # Serial Port Connection Timers
         self.connection_timer = QTimer(self)
@@ -64,8 +88,92 @@ class MainWindow(QMainWindow):
         self.signaller = Signaller()
         self.signaller.port_scan_complete.connect(self.update_ports_list)
 
+        # Graceful Close
         QCoreApplication.instance().aboutToQuit.connect(self.close_window)
         
+        # Init Data
+        self.configData = ControllerConfig(VERSION, [])
+
+    def add_appliance(self):
+        """
+        Adds an appliance to the system
+        """
+        # Get the appliance name
+        name = self.addApplianceNameT.toPlainText()
+        if name == "":
+            print("No Appliance Name Set!")
+            return
+        
+        if len(self.configData.appliances) >= 4:
+            print("Appliance Limit Reached!")
+            return
+
+        # Create the appliance
+        self.configData.appliances.append(generate_base_appliance(name))
+        self.addApplianceNameT.clear()
+
+        # Refresh UI
+        self.reload_configuration_data()
+
+    def reload_configuration_data(self, applianceName=None, applianceIndex=None, controlIndex=None):
+        """
+        Reload the configuration currently stored in memory into the GUI
+        Can be used to reload the GUI configuration file after a major change has been made (such as adding a profile).
+        NOTE: you should provide either an applianceName or applianceIndex to restore the existing state of the application.
+              If both are provided the profile_name will be used by default.
+        """
+        data = self.configData
+        # Set appliance state
+        if applianceName is not None:
+            # Set this profile to the current selection
+            index = 0
+            for appliance in data.appliances:
+                if appliance.name == applianceName:
+                    index = data.appliances.index(appliance)
+            self.currentApplianceIndex = index
+        elif applianceIndex is not None:
+            self.currentApplianceIndex = applianceIndex
+        else:
+            self.currentApplianceIndex = 0
+
+        # Re-Populate GUI Fields
+        # Appliance Selector
+        self.selectedApplianceC.clear()
+        for i in range(len(data.appliances)):
+            self.selectedApplianceC.addItem(data.appliances[i].name)
+
+        # Appliance Global Settings
+        self.orientationC.setCurrentIndex(data.appliances[self.currentApplianceIndex].orientation)
+        
+        # Controls
+        for idx, control in enumerate(data.appliances[self.currentApplianceIndex].controls):
+            self.update_control_ui(idx, control.label, control.colour)
+
+    def update_control_ui(self, index, label, colour):
+        """
+        Update the UI for a control based on the provided index, label & colour.
+        NOTE: Controls are labelled 1-6, corresponding indexes are 0-based i.e 0-5
+        """
+        colourStr = f"rgb({colour[0]}, {colour[1]}, {colour[2]})"
+        styleSheet = f"border-radius:20px; background-color: {colourStr}; font: 75 12pt 'MS Shell Dlg 2';"
+        if index == 0:
+            self.controlB_1.setStyleSheet(styleSheet)
+            self.controlB_1.setText(label)
+        elif index == 1:
+            self.controlB_2.setStyleSheet(styleSheet)
+            self.controlB_2.setText(label)
+        elif index == 2:
+            self.controlB_3.setStyleSheet(styleSheet)
+            self.controlB_3.setText(label)
+        elif index == 3:
+            self.controlB_4.setStyleSheet(styleSheet)
+            self.controlB_4.setText(label)
+        elif index == 4:
+            self.controlB_5.setStyleSheet(styleSheet)
+            self.controlB_5.setText(label)
+        elif index == 5:
+            self.controlB_6.setStyleSheet(styleSheet)
+            self.controlB_6.setText(label)
 
     def fetch_serial(self):
         """
---
+++ pc_software/smart_ir_data.py
@@ -0,0 +1,55 @@
+"""
+Data structures for the Smart-Ir: Universal Remote Control System
+Written by AB
+"""
+from dataclasses import dataclass
+from typing import List
+
+# Constants
+VERSION = 1.0 # Version of the configuration data
+ORIENTATION_UP = 0
+ORIENTATION_RIGHT = 1
+ORIENTATION_DOWN = 2
+ORIENTATION_LEFT = 3
+
+@dataclass
+class Control:
+    """
+    IR Control to transmit
+    """
+    label: str # control label
+    irCode: bytes # bytes to transmit
+    irCodeLen: int # number of bytes to transmit
+    colour: List[int] # control colour
+
+
+@dataclass
+class Appliance:
+    """
+    Appliance data within the configuration file
+    """
+    name: str # Appliance name
+    orientation: int # Appliance orientation 0-3 (see constants section)
+    controls: List[Control] # controls for the associated appliance
+
+@dataclass
+class ControllerConfig:
+    """
+    The Configuration data transmitted to the M5Core2 Universal Remote Controllers
+    """
+    version: float
+    appliances: List[Appliance] # appliances within the configuration
+
+def generate_base_appliance(name):
+    """
+    Generates a base appliance with default values
+    """
+    controls = []
+    for i in range(6):
+        # Add basic controls
+        control = Control(f"Control {i}", bytes.fromhex("FFFFFF"), 3, [0, 0, 255])
+        controls.append(control)
+
+    appliance = Appliance(name, 0, controls)
+
+    return appliance
