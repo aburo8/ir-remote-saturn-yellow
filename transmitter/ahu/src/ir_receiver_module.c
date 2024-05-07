7904580d6626caf7dae74185ada4cdead845eb31
Alexander B <abwork2071@gmail.com>
2024-05-07 10:48:36 +1000

    [AHU] Working NEC code receiving

--- transmitter/ahu/src/ir_receiver_module.c
+++ transmitter/ahu/src/ir_receiver_module.c
@@ -23,13 +23,123 @@ LOG_MODULE_REGISTER(ir_receiver_module);
 
 static const struct gpio_dt_spec ir_rec = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, ts1838_gpios);
 
-static void ir_signal_callback(const struct device *dev, struct gpio_callback *cb,
-                               uint32_t pins)
-{
-    // Handle IR signal detection here
-    LOG_INF("IR signal detected!");
+// Define IR signal state and data structures
+static uint32_t ir_data = 0;
+static uint16_t pulseCount = 0;
+static uint8_t timerMs = 0;
+
+#define TIMER_TIMEOUT 150 // The timer is automatically stopped if it ticks for more than 150ms
+
+// Function Prototypes
+void increment_timer(struct k_work *work);
+
+// System worker thread to execute timer operations
+K_WORK_DEFINE(my_work, increment_timer);
+
+/**
+ * Timer handler which triggers when the timer expires
+ * Calls increment_timer to perform the counting operation
+ * 
+ * Returns: void
+ * 
+*/
+static void ir_timer_step_handler(struct k_timer *timer_id) {
+    k_work_submit(&my_work);
 }
 
+/**
+ * Resets the timer when it is stopped
+ * 
+ * Returns: void
+ * 
+*/
+static void ir_timer_stop_handler(struct k_timer *timer_id) {
+    // If the timer times out then reset the pulse count
+    pulseCount = 0;
+    timerMs = 0;
+    LOG_INF("Timer Stopped");
+}
+
+// IR timeout timer to track the time differences between IR pulses
+K_TIMER_DEFINE(ir_timeout_timer, ir_timer_step_handler, ir_timer_stop_handler);
+
+/**
+ * Increments the ir_timeout_timer
+ * 
+ * Returns: void
+*/
+void increment_timer(struct k_work *work)
+{
+    if (timerMs >= TIMER_TIMEOUT) {
+        LOG_INF("Timer Timed Out");
+        k_timer_stop(&ir_timeout_timer);
+    }
+    timerMs++;
+}
+
+/**
+ * Checks to see if the provided pulse is a data bit
+ * 
+ * Returns: True if the pulse is a data pulse, False otherwise
+ * 
+*/
+static bool is_bit_pulse(uint16_t pulse) {
+    return 3 <= pulse && pulse <= 34;
+}
+
+/**
+ * Checks to see if the provided pulse is the end of a message
+ * 
+ * Returns: True if the message is complete, False otherwise
+ * 
+*/
+static bool is_message_pulse(uint16_t pulse) {
+    return pulse == 34;
+}
+
+/**
+ * GPIO Callback function - executes on falling GPIO falling edge triggers
+ * 
+ * Returns: void
+ * 
+*/
+static void ir_signal_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+    // Check if the GPIO pin triggered the callback
+    if (pins & BIT(TS1838_GPIO_PIN)) {
+        // Read IR Receiver Pin
+        int pin_value = gpio_pin_get(dev, TS1838_GPIO_PIN);
+        if (pin_value < 0) {
+            LOG_ERR("Error reading GPIO pin: %d", pin_value);
+            return;
+        }
+        pulseCount++;
+
+        // Check if the current falling edge is a bit pulse
+        if (is_bit_pulse(pulseCount)) {
+            // Decode Bit Value
+            ir_data <<= 1;
+            printk("Pulse Count: %d, Timer: %d\n", pulseCount, timerMs);
+            ir_data += (timerMs >= 2) ? 1 : 0;
+        }
+
+        // Measure
+        if (is_message_pulse(pulseCount)) {
+            // TODO: save to a queue/send to PC
+            k_timer_stop(&ir_timeout_timer);
+            LOG_INF("IR Data 0x%x", ir_data);
+        }
+
+        timerMs = 0;
+        k_timer_start(&ir_timeout_timer, K_MSEC(1), K_MSEC(1));
+    }
+}
+
+/**
+ * Initialises the IR Receiver
+ * 
+ * Returns: 0 on success
+ * 
+*/
 int init_ir_receiver() {
 
     uint8_t ret = 0;
@@ -62,7 +172,7 @@ int init_ir_receiver() {
     }
 
     // Enable interrupt on rising edge (or falling edge, depending on IR sensor)
-    ret = gpio_pin_interrupt_configure_dt(&ir_rec, GPIO_INT_EDGE_BOTH);
+    ret = gpio_pin_interrupt_configure_dt(&ir_rec, GPIO_INT_EDGE_FALLING);
     if (ret < 0) {
         LOG_ERR("Failed to configure interrupt: %d", ret);
         return ret;
