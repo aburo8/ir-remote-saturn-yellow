f7f72d9ebe6083a5b48fa6542c57822bf796a3ca
Benjamin <bldterrace@gmail.com>
2024-05-17 17:58:14 +1000

    Working IR transmitter and decoder code for arduino

--- controllers/IRDecoder_arduino/IRDecoder_arduino.ino
+++ controllers/IRDecoder_arduino/IRDecoder_arduino.ino
@@ -1,4 +1,4 @@
-
+#include <M5Core2.h>
 #include <IRremote.h>
 
 int IRPIN = 26;
@@ -6,14 +6,22 @@ int IRPIN = 26;
 void setup()
 
 {
-
-Serial.begin(9600);
-
 Serial.println("Enabling IRin");
 
 IrReceiver.begin(IRPIN, ENABLE_LED_FEEDBACK);
 
 Serial.println("Enabled IRin");
+
+M5.begin();
+M5.Lcd.setTextSize(2);
+M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+M5.Lcd.fillScreen(TFT_BLACK);
+// Display a message on the screen
+M5.Lcd.setCursor(10, 10);
+M5.Lcd.print("Ready to recieve");
+
+// Start the serial communication for debugging
+Serial.begin(115200);
 
 }
 
@@ -26,7 +34,12 @@ if (IrReceiver.decode())
 {
 
 Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-
+// Display the transmitted code on the screen
+M5.Lcd.fillScreen(TFT_BLACK);
+M5.Lcd.setCursor(10, 10);
+M5.Lcd.printf("Transmitted: %08X", IrReceiver.decodedIRData.decodedRawData);
+//M5.Lcd.printf("Protocol: %s\n", decode_type_str(results.decode_type));
+//M5.Lcd.printf("IR Code: 0x%08X", IrReceiver.decodedIRData.decodedRawData);
 IrReceiver.resume();
 
 }
