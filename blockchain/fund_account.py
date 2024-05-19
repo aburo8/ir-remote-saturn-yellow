c68879278a114c0630a83b0184f54bd2cd14cfd9
Alexander B <abwork2071@gmail.com>
2024-05-19 18:39:35 +1000

    [Blockchain] Updated fund account script

--- blockchain/fund_account.py
+++ blockchain/fund_account.py
@@ -1,7 +1,7 @@
 from web3 import Web3, HTTPProvider
-from addr_book import controller1_addr
+from addr_book import controller1_addr, controller2_addr, pc_addr
 
-addrToFunc = controller1_addr
+addrToFunc = pc_addr
 
 w3 = Web3(HTTPProvider("http://localhost:8545"))
 
@@ -18,7 +18,7 @@ acct2 = w3.eth.account.from_key(pk)
 # For the sake of this example, fund the new account:
 tx_hash = w3.eth.send_transaction({
     "from": "0x123463a4B065722E99115D6c222f267d9cABb524",
-    "value": w3.to_wei(100, 'ether'),
+    "value": w3.to_wei(500, 'ether'),
     "to": acct2.address
 })
 
